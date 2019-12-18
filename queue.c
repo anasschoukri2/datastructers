d40564d0032cb39927d5d22d6f08057c0885957a
anasschoukri2 <38406227+anasschoukri2@users.noreply.github.com>
2019-12-18 16:47:31 +0100

    Create queue.c

---
+++ queue.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct N {
+    int data;
+    struct N* next;
+} node;
+void push(node* a, node** queue)
+{
+    if (*queue == NULL) {
+        *queue = a;
+        return;
+    }
+    node* walker = *queue;
+    while (walker->next != NULL) {
+        walker = walker->next;
+    }
+    walker->next = a;
+}
+void pop(node** queue)
+{
+    if (*queue == NULL) {
+        return;
+    }
+    if ((*queue)->next == NULL) {
+        printf("%d ", (*queue)->data);
+        *queue = NULL;
+        return;
+    }
+    node* walker = *queue;
+    node* rear = *queue;
+    while (walker->next != NULL) {
+        rear = walker;
+        walker = walker->next;
+    }
+    printf("%d ", walker->data);
+    rear->next = NULL;
+}
+int main(void)
+{
+    node* queue = (node*)malloc(sizeof(node));
+    queue = NULL;
+    node* a = (node*)malloc(sizeof(node));
+    a->data = 1;
+    a->next = NULL;
+    push(a, &queue);
+    a = (node*)malloc(sizeof(node));
+    a->data = 2;
+    a->next = NULL;
+    push(a, &queue);
+    a = (node*)malloc(sizeof(node));
+    a->data = 3;
+    a->next = NULL;
+    push(a, &queue);
+    pop(&queue);
+    pop(&queue);
+    pop(&queue);
+    pop(&queue);
+    return 0;
+}
